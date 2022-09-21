b2bde71769307039bac2ee60381ff7d6bcc4e1e9
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2022-09-21 12:25:57 +0530

    Test-app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+const initialPrice = document.querySelector("#initial-price");
+const stocksQuantity = document.querySelector("#stocks-quantity");
+const currentPrice = document.querySelector("#current-price");
+const submitButton = document.querySelector("#submit-btn");
+const outputBox = document.querySelector("#output");
+
+function calculateProfitAndLoss(initial, quantity, current){
+    if(initial > current){
+        var loss = ((initial - current) * quantity).toFixed(2);
+        var lossPercentage = (loss/initial) *100;
+        showOutput( `Hey the loss is ${loss} and the percent is ${lossPercentage}%`);
+
+    }
+    else if(current > initial){
+        var profit = ((current - initial) * quantity).toFixed(2);
+        var profitPercentage = (profit / initial) * 100;
+        showOutput( `Hey the profit is ${profit} and the percent is ${profitPercentage}%`);
+
+    }
+    else{
+        showOutput( `No Pain No gain`);
+
+    }
+}
+
+function submitHandler(){
+    var ip = initialPrice.value;
+    var qty = stocksQuantity.value;
+    var curr = currentPrice.value;
+    calculateProfitAndLoss(ip, qty, curr);
+}
+
+function showOutput(message, status){
+    outputBox.innerHTML = message;
+}
+
+submitButton.addEventListener("click", submitHandler)
